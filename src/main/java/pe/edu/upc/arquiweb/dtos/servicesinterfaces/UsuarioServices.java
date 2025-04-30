3501e50c2e56fdd5b55c410c3d1b33f2c75ada41
Adrian Meneses Salazar <u20221c520@upc.edu.pe>
2025-04-29 19:58:14 -0500

    finalito2

--- src/main/java/pe/edu/upc/arquiweb/dtos/servicesinterfaces/UsuarioServices.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upc.arquiweb.dtos.servicesinterfaces;
-
-import pe.edu.upc.demog1.entities.Usuario;
-
-import java.util.List;
-public interface UsuarioServices {
-    public List<Usuario> list() ;
-}
