9cf6af287b3ba9664f4b9b3ee2c3e1fbf8f305ee
RonyL25 <u202224637@upc.edu.pe>
2025-05-06 13:20:34 -0500

    mas

--- src/main/java/pe/edu/upc/arquiweb/controllers/UsuarioController.java
+++ src/main/java/pe/edu/upc/arquiweb/controllers/UsuarioController.java
@@ -2,10 +2,9 @@ package pe.edu.upc.arquiweb.controllers;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.arquiweb.dtos.UsuarioDTO;
+import pe.edu.upc.arquiweb.entities.Usuario;
 import pe.edu.upc.arquiweb.servicesinterfaces.IUsuarioService;
 
 import java.util.List;
@@ -23,5 +22,24 @@ public class UsuarioController {
             ModelMapper m= new ModelMapper();
             return m.map(u,UsuarioDTO.class);
         }).collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public void insertar(@RequestBody UsuarioDTO dto) {
+        ModelMapper m = new ModelMapper();
+        Usuario u = m.map(dto, Usuario.class);
+        uS.insert(u);
+    }
+
+    @PutMapping
+    public void modificar(@RequestBody UsuarioDTO dto) {
+        ModelMapper m = new ModelMapper();
+        Usuario u = m.map(dto, Usuario.class);
+        uS.update(u);
+    }
+
+    @DeleteMapping("/eliminar{id}")
+    public void eliminar(@PathVariable("id") int id) {
+        uS.delete(id);
     }
 }
