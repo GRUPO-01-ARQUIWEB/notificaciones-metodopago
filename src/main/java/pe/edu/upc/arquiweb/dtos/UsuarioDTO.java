a70c7e20c40bfec53bce49efdf658d0dee78973d
RonyL25 <u202224637@upc.edu.pe>
2025-05-09 16:14:17 -0500

    actuali

--- src/main/java/pe/edu/upc/arquiweb/dtos/UsuarioDTO.java
+++ src/main/java/pe/edu/upc/arquiweb/dtos/UsuarioDTO.java
@@ -3,35 +3,15 @@ package pe.edu.upc.arquiweb.dtos;
 public class UsuarioDTO {
 
     private int idUsuario;
-
-    private String nombre;
-
+    private String username;
+    private String password;
+    private Boolean enabled;
     private String correo;
-
-    private String contrasenahash;
-
     private String direccion;
-
     private String telefono;
-
-    private String rolClAd;
-
     private double latitud;
-
     private double longitud;
 
-    /*
-    private Rol rol;
-
-    public Rol getRol() {
-        return rol;
-    }
-
-    public void setRol(Rol rol) {
-        this.rol = rol;
-    }
-
-    */
     public int getIdUsuario() {
         return idUsuario;
     }
@@ -40,12 +20,28 @@ public class UsuarioDTO {
         this.idUsuario = idUsuario;
     }
 
-    public String getNombre() {
-        return nombre;
+    public String getUsername() {
+        return username;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
     }
 
     public String getCorreo() {
@@ -54,14 +50,6 @@ public class UsuarioDTO {
 
     public void setCorreo(String correo) {
         this.correo = correo;
-    }
-
-    public String getContrasenahash() {
-        return contrasenahash;
-    }
-
-    public void setContrasenahash(String contrasenahash) {
-        this.contrasenahash = contrasenahash;
     }
 
     public String getDireccion() {
@@ -78,14 +66,6 @@ public class UsuarioDTO {
 
     public void setTelefono(String telefono) {
         this.telefono = telefono;
-    }
-
-    public String getRolClAd() {
-        return rolClAd;
-    }
-
-    public void setRolClAd(String rolClAd) {
-        this.rolClAd = rolClAd;
     }
 
     public double getLatitud() {
---
+++ src/main/java/pe/edu/upc/arquiweb/dtos/UsuarioRegistroDTO.java
@@ -0,0 +1,41 @@
+package pe.edu.upc.arquiweb.dtos;
+
+public class UsuarioRegistroDTO {
+
+    private String username;
+    private String correo;
+    private String direccion;
+    private String telefono;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
