1f1560d66b9de2f1121101c6b4a82b633c02374f
Adrian Meneses Salazar <u20221c520@upc.edu.pe>
2025-05-13 19:51:21 -0500

    Update CarritoCompraController.java

--- src/main/java/pe/edu/upc/arquiweb/controllers/CarritoCompraController.java
+++ src/main/java/pe/edu/upc/arquiweb/controllers/CarritoCompraController.java
@@ -19,7 +19,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("CarritoCompra")
 public class CarritoCompraController {
-  @Autowired
+    @Autowired
     private ICarritoCompraServices uS;
 
 
